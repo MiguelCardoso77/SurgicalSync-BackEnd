4d06d34ee557d62b0c72725eaae379e241442072
Miguel <1220772@isep.ipp.pt>
2024-10-04 14:38:56 +0100

    #22 Level1, 2 and 3 documentation. Implementing value objects according to documentation.

--- Domain/OperationTypes/RequiredStaff.cs
+++ Domain/OperationTypes/RequiredStaff.cs
@@ -1,7 +1,36 @@
-﻿namespace DDDSample1.Domain.OperationTypes
+﻿using DDDSample1.Domain.Shared;
+
+namespace DDDNetCore.Domain.OperationTypes
 {
-    public class RequiredStaff
+    public class RequiredStaff : IValueObject
     {
+        public string RequiredStaffValue { get; private set; }
+        
+        private RequiredStaff() { }
+        
+        public RequiredStaff(string requiredStaff)
+        {
+            this.RequiredStaffValue = requiredStaff;
+        }
+        
+        public override string ToString()
+        {
+            return RequiredStaffValue;
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if (obj is RequiredStaff other)
+            {
+                return RequiredStaffValue == other.RequiredStaffValue;
+            }
+            return false;
+        }
+        
+        public override int GetHashCode()
+        {
+            return RequiredStaffValue.GetHashCode();
+        }
         
     }
 }
