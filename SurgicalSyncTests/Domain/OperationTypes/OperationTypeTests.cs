d377f35cc17f0232ac81d4b78ce363f1fa600a82
Miguel <1220772@isep.ipp.pt>
2024-10-07 15:41:42 +0100

    #20 Tests concluded for this use case.

---
+++ SurgicalSyncTests/Domain/OperationTypes/OperationTypeTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using DDDNetCore.Domain.OperationTypes;
+using NUnit.Framework;
+
+namespace DDDNetCore.SurgicalSyncTests.Domain.OperationTypes
+{
+    [TestFixture]
+    public class OperationTypeTests
+    {
+        [Test]
+        public void TestConstructor()
+        {
+            var operationType = new OperationType(
+                new OperationTypeId("1"),
+                new OperationName("Surgery"),
+                new List<RequiredStaff>(),
+                new List<EstimatedDuration>()
+            );
+            
+            Assert.AreEqual("1", operationType.Id.AsString());
+            Assert.AreEqual("Surgery", operationType.Name.OperationNameValue);
+            Assert.AreEqual(0, operationType.RequiredStaff.Count);
+            Assert.AreEqual(0, operationType.EstimatedDuration.Count);
+        }
+        
+    }
+}
