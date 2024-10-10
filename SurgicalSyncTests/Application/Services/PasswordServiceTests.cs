e47b1cbb33069ead1c22de967dc3d3be748a17c0
Miguel <1220772@isep.ipp.pt>
2024-10-10 09:18:50 +0100

    #1 Tests for PasswordService

---
+++ SurgicalSyncTests/Application/Services/PasswordServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using DDDNetCore.Application.Services;
+using NUnit.Framework;
+
+namespace DDDNetCore.SurgicalSyncTests.Application.Services
+{
+    [TestFixture]
+    public class PasswordServiceTests
+    {
+        [Test]
+        public void TestGeneratePassword()
+        {
+            // Arrange
+            const int passwordLength = 10;
+            
+            // Act
+            var password = PasswordService.GeneratePassword();
+            
+            // Assert
+            Assert.AreEqual(passwordLength, password.Length);
+            Assert.IsTrue(password.Any(char.IsDigit));
+            Assert.IsTrue(password.Any(char.IsUpper));
+            Assert.IsTrue(password.Any(char.IsSymbol));
+        }
+    }
+}
