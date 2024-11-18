05639ec7f381a682fc284b807f5d3b1b815b874d
DiogoMPR <diogo.mp.ribeiro2004@gmail.com>
2024-11-18 15:07:09 +0000

    Changing the http and https links

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace DDDNetCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001");
+        //.UseStartup<Startup>();
     }
 }
