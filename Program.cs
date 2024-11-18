868a7357d366afe0b67aef4872db5bc65acfc359
DiogoMPR <diogo.mp.ribeiro2004@gmail.com>
2024-11-18 15:25:07 +0000

    Reverting changes

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ namespace DDDNetCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001")
                 .UseStartup<Startup>();
     }
 }
