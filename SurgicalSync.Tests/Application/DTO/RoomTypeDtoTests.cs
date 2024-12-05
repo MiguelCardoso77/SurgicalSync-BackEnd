7e3b60b7fb6ee94d92e75f1278c70891449d2c5b
Miguel <1220772@isep.ipp.pt>
2024-12-05 17:21:56 +0000

    RoomType Testing start.

---
+++ SurgicalSync.Tests/Application/DTO/RoomTypeDtoTests.cs
@@ -0,0 +1,40 @@
+﻿using DDDNetCore.Application.DTO;
+using NUnit.Framework;
+
+namespace DDDNetCore.SurgicalSync.Tests.Application.DTO;
+
+[TestFixture]
+public class RoomTypeDtoTests
+{
+    [Test]
+    public void TestCreateIncompletePlanningDto()
+    {
+        // Arrange
+        var dto = new RoomTypeDto()
+        {
+            RoomTypeCode = "123--321",
+            RoomTypeDescription = "Surgery"
+        };
+        
+        // Assert
+        Assert.AreEqual(dto.RoomTypeCode, "123--321");
+        Assert.AreEqual(dto.RoomTypeDescription, "Surgery");
+    }
+    
+    [Test]
+    public void TestCreateCompletePlanningDto()
+    {
+        // Arrange
+        var dto = new RoomTypeDto()
+        {
+            RoomTypeCode = "123--321",
+            RoomTypeDesignation = "ICU",
+            RoomTypeDescription = "Surgery"
+        };
+        
+        // Assert
+        Assert.AreEqual(dto.RoomTypeCode, "123--321");
+        Assert.AreEqual(dto.RoomTypeDesignation, "ICU");
+        Assert.AreEqual(dto.RoomTypeDescription, "Surgery");
+    }
+}
