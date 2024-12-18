cf8f1adca69de2ecee84aa59e26e59ab5b1cdb36
Miguel <1220772@isep.ipp.pt>
2024-12-18 11:40:30 +0000

    ResetPasswordDtoTests.cs

---
+++ SurgicalSync.Tests/Application/DTO/ResetPasswordDtoTests.cs
@@ -0,0 +1,37 @@
+﻿using DDDNetCore.Application.DTO;
+using NUnit.Framework;
+
+namespace DDDNetCore.SurgicalSync.Tests.Application.DTO;
+
+[TestFixture]
+public class ResetPasswordDtoTests
+{
+    [Test]
+    public void TestCreateIncompleteResetPasswordDto()
+    {
+        // Arrange
+        var dto = new ResetPasswordDto()
+        {
+            UserEmail = "1220772@isep.ipp.pt",
+            NewPassword = "123456"
+        };
+        
+        // Assert
+        Assert.AreEqual(dto.UserEmail, "1220772@isep.ipp.pt");
+        Assert.AreEqual(dto.NewPassword, "123456");
+    }
+    
+    [Test]
+    public void TestCreateCompleteForgotPasswordDto()
+    {
+        // Arrange
+        var dto = new ForgotPasswordDto()
+        {
+            UserEmail = "1220772@isep.ipp.pt"
+        };
+        
+        // Assert
+        Assert.AreEqual(dto.UserEmail, "1220772@isep.ipp.pt");
+    }
+    
+}
