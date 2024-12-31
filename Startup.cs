142422e1fc7ecf4c507799393507fe46d9a2970b
Miguel <1220772@isep.ipp.pt>
2024-12-31 11:25:55 +0000

    Changing CORS Policy.

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,7 @@ namespace DDDNetCore
         {
             services.AddCors(options =>
             {
-                options.AddPolicy("AllowOrigin", builder => builder.WithOrigins(new[] { "http://localhost:63359", "http://localhost:4200", "https://surgical-sync-front-end.vercel.app" }).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+                options.AddPolicy("AllowOrigin", builder => builder.WithOrigins(new[] { "https://miguelcardoso77.github.io", "http://localhost:4200", "https://surgical-sync-front-end.vercel.app" }).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
             });
             
             FirebaseApp.Create(new AppOptions()
